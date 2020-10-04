e968bd1ed9fe61f8ce128f8d552ca7b2f6096a46
hamza <hnael65@gmail.com>
2020-10-04 17:57:04 +0300

    work

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 
 const Jikan = require('jikan-node');
 const mal = new Jikan();
-
+const { SitemapStream, streamToPromise } = require( 'sitemap' );
+  const { Readable } = require( 'stream' );
+  const { createGzip } = require('zlib')
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -796,6 +798,45 @@ res.render("editAnime");
 app.get('/deleteAnimefortheadminsOnly', (req, res) => {
 res.render("/deletAnime");	
 });
+
+
+let sitemap;
+ 
+app.get('/sitemap.xml', function(req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+  // if we have a cached entry send it
+  if (sitemap) {
+    res.send(sitemap);
+    return;
+  }
+ 
+  try {
+    const smStream = new SitemapStream({ hostname: 'https://mhanime.herokuapp.com/' });
+    const pipeline = smStream.pipe(createGzip());
+
+    // pipe your entries or directly write them.
+    smStream.write({ url: '/page-1/',  changefreq: 'daily', priority: 0.3 });
+    smStream.write({ url: '/page-2/',  changefreq: 'monthly',  priority: 0.7 });
+    
+    /* or use
+    Readable.from([{url: '/page-1'}...]).pipe(smStream)
+    if you are looking to avoid writing your own loop.
+    */
+ 
+    // cache the response
+    streamToPromise(pipeline).then(sm => sitemap = sm);
+    // make sure to attach a write stream such as streamToPromise before ending
+    smStream.end()
+    // stream write the response
+    pipeline.pipe(res).on('error', (e) => {throw e;})
+  } catch (e) {
+    console.error(e);
+    res.status(500).end();
+  }
+})
+
+
 app.listen(port || 3000, function () {
     console.log("system is work on" + 3000);
 });
